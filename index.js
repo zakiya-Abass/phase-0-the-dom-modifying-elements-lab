f0a4f2af7f65ccfc020282d9dc51b1ad04a8c0fd
Zakiya-Abass <zakiyaabass@icloud.com>
2022-12-31 03:20:37 +0300

    DOM get element

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+document.getElementById("main").remove();
+const newHeader = document.createElement("H1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Zakiya is the champion";
