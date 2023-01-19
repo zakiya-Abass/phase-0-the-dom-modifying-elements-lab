65a4d7b6b3a24dc3e72aebcf34e044390110da5c
Zakiya-Abass <zakiyaabass@icloud.com>
2023-01-19 16:11:56 +0300

    dom modiffying elements

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ document.getElementById("main").remove();
 const newHeader = document.createElement("H1");
 newHeader.id = "victory";
 newHeader.innerHTML = "Zakiya is the champion";
+
+//no longer has DOM node 'main#main'
+//✓ has a 'newHeader' variable that points to an <h1> node
+//✓ the 'newHeader' variable that points to the <h1> node has an id of 'victory'
+//✓ the 'newHeader' variable that points to the <h1> node with an id of 'victory' has the text "YOUR-NAME is the champion" (with your name in place of YOUR-NAME) inside it
